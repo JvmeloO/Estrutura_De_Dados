796f4b13a97a8b5bb70dcf9761a814818bf19bad
João melo <jvmelo100@hotmail.com>
2020-10-12 21:45:36 -0300

    Mudança do nome da instância da lista dupla.

--- Lista_DuplamenteEncadeada/Lista_DuplamenteEncadeada/MainClass.cs
+++ Lista_DuplamenteEncadeada/Lista_DuplamenteEncadeada/MainClass.cs
@@ -6,34 +6,34 @@ namespace Estrutura_de_Dados_Vetor
     {
         static void Main(string[] args)
         {
-            var listaSimples = new Lista_Dupla<Aluno>();
+            var listaDupla = new Lista_Dupla<Aluno>();
             var aluno1 = new Aluno("Rodrigo", 1, 15);
             var aluno2 = new Aluno("Joao", 2, 15);
             var aluno3 = new Aluno("Carla", 3, 14);
             var aluno4 = new Aluno("Alan", 4, 14);
-            listaSimples.AdicionaFim(aluno1);
-            listaSimples.AdicionaFim(aluno2);
-            listaSimples.AdicionaFim(aluno3);
-            listaSimples.AdicionaFim(aluno4);
-            listaSimples.AdicionaInicio(aluno2);
-            listaSimples.AdicionaInicio(aluno3);
-            listaSimples.AdicionaInicio(aluno4);
+            listaDupla.AdicionaFim(aluno1);
+            listaDupla.AdicionaFim(aluno2);
+            listaDupla.AdicionaFim(aluno3);
+            listaDupla.AdicionaFim(aluno4);
+            listaDupla.AdicionaInicio(aluno2);
+            listaDupla.AdicionaInicio(aluno3);
+            listaDupla.AdicionaInicio(aluno4);
             //listaSimples.AdicionaFim(aluno3);
             //listaSimples.AdicionaFim(aluno3);
-            listaSimples.Adiciona(2, aluno1);
-            listaSimples.Remove(2);
-            listaSimples.RemoveInicio();
-            listaSimples.RemoveFim();
+            listaDupla.Adiciona(2, aluno1);
+            listaDupla.Remove(2);
+            listaDupla.RemoveInicio();
+            listaDupla.RemoveFim();
             //listaSimples.Adiciona(2, aluno4);
             //listaSimples.RemoveInicio();
-            Console.WriteLine(listaSimples.Vazio());
+            Console.WriteLine(listaDupla.Vazio());
             //vetor.Limpar();
             //Console.WriteLine(vetor.Vazio());
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("\n");
                 Console.WriteLine("Aluno "+i);
-                var aluno = listaSimples.Recupera(i);
+                var aluno = listaDupla.Recupera(i);
                 Console.WriteLine(aluno.GetNome());
                 Console.WriteLine(aluno.GetIdade());
                 Console.WriteLine(aluno.GetAno());
