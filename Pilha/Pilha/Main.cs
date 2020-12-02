a6575e725999d38386bcf4d11898f40c36816989
João melo <jvmelo100@hotmail.com>
2020-12-02 18:18:57 -0300

    Adição do projeto da Pilha Genérica.

---
+++ Pilha/Pilha/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilha
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var Pilha = new Pilha<Aluno>(3);
+            var aluno1 = new Aluno("Rodrigo", 1, 15);
+            var aluno2 = new Aluno("Joao", 2, 15);
+            var aluno3 = new Aluno("Carla", 3, 14);
+            var aluno4 = new Aluno("Alan", 4, 14);
+            Pilha.Push(aluno1);
+            Pilha.Push(aluno2);
+            Pilha.Pull(aluno3);
+            Pilha.Push(aluno4);
+            Pilha.Pop();
+            //Pilha.Limpa();
+            Console.WriteLine(Pilha.Top().GetNome());
+            Console.WriteLine(Pilha.Top().GetIdade());
+            Console.WriteLine(Pilha.Top().GetAno());
+            Console.WriteLine(Pilha.ExisteDado());
+            Console.WriteLine(Pilha.IsEmpty());
+            Console.WriteLine(Pilha.Tamanho());            
+        }
+    }
+}
---
+++ Pilha/Pilha/Pilha.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilha
+{
+    public class Pilha<T>
+    {
+        private T[] TPilha;
+        private int Quantidade = 0;
+        private int TopoPilha = 0;
+
+        public Pilha(int Tamanho) 
+        { 
+            TPilha = new T[Tamanho];
+            TopoPilha = Tamanho;
+        }
+
+        public void Push(T elemento)
+        {
+            TPilha[Quantidade] = elemento;
+            Quantidade++;
+        }
+
+        public bool ExisteDado()
+        {
+            if (TopoPilha != Quantidade)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsEmpty() 
+        {
+            if (Quantidade != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public T Top() 
+        {
+            if (IsEmpty() == true)
+            {
+                throw new Exception("Pilha vazia!");
+            }
+
+            return TPilha[Quantidade-1];
+        }
+
+        public void Pull(T Elemento) 
+        {
+            if (IsEmpty() == true)
+            {
+                throw new Exception("Pilha vazia!");
+            }
+
+            TPilha[Quantidade-1] = Elemento;
+        }
+
+        public void Pop() 
+        {
+            if (IsEmpty() == true)
+            {
+                throw new Exception("Pilha vazia!");
+            }
+
+            TPilha[Quantidade-1] = (T)Activator.CreateInstance(typeof(T));
+            Quantidade--;
+        }
+
+        public int Tamanho() => Quantidade;
+
+        public void Limpa() 
+        {
+            Quantidade = 0;
+            TPilha = new T[TopoPilha];
+        }
+    }
+}
