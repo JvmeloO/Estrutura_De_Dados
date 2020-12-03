434ef3e5ad678a638277a4e2f2a827a5c643e17a
João melo <jvmelo100@hotmail.com>
2020-12-03 20:56:09 -0300

    Adição do projeto de Fila ao github

---
+++ Fila/Fila/Fila.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilha
+{
+    public class Fila<T>
+    {
+        private T[] TFila;
+        private int Quantidade = 0;
+        private int TamanhoFila = 0;
+
+        public Fila(int Tamanho) 
+        {
+            TFila = new T[Tamanho];
+            TamanhoFila = Tamanho;
+        }
+
+        public void Inserir(T elemento)
+        {
+            TFila[Quantidade] = elemento;
+            Quantidade++;
+        }
+
+        public bool ExisteDado()
+        {
+            if (TFila[0] == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsEmpty() 
+        {
+            if (Quantidade != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public T Recuperar() 
+        {
+            if (IsEmpty() == true)
+            {
+                throw new Exception("Fila vazia!");
+            }
+
+            return TFila[0];
+        }
+
+        public void Alterar(T Elemento) 
+        {
+            if (IsEmpty() == true)
+            {
+                throw new Exception("Fila vazia!");
+            }
+
+            TFila[0] = Elemento;
+        }
+
+        public void Remover() 
+        {
+            var NovaFila = new T[TamanhoFila];
+            for (int i = 1; i < Quantidade; i++)
+            {
+                NovaFila[i-1] = TFila[i];
+            }
+            TFila = NovaFila;
+        }
+
+        public int Tamanho() => Quantidade;
+
+        public void Limpar() 
+        {
+            Quantidade = 0;
+            TFila = new T[TamanhoFila];
+        }
+    }
+}
---
+++ Fila/Fila/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilha
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var Fila = new Fila<Aluno>(3);
+            var aluno1 = new Aluno("Rodrigo", 1, 15);
+            var aluno2 = new Aluno("Joao", 2, 15);
+            var aluno3 = new Aluno("Carla", 3, 14);
+            var aluno4 = new Aluno("Alan", 4, 14);
+            Fila.Inserir(aluno1);
+            Fila.Inserir(aluno2);
+            Fila.Inserir(aluno4);
+            //Fila.Alterar(aluno3);
+            Fila.Remover();
+            Console.WriteLine(Fila.Recuperar().GetNome());
+            Console.WriteLine(Fila.Recuperar().GetIdade());
+            Console.WriteLine(Fila.Recuperar().GetAno());
+            Console.WriteLine(Fila.ExisteDado());
+            Console.WriteLine(Fila.IsEmpty());
+        }
+    }
+}
